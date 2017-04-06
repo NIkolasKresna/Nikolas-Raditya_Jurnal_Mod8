2a3ae77af124a1a7925f73fa9eb8e38f4d1db0b5
NIkolasKresna <Nicolasradityakh@gmail.com>
2017-04-06 09:00:14 +0700

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -16,6 +16,7 @@ public class Fibonacci {
         for (int i = 0; i < n; i++) {
             System.out.print(fib[i] +  " ");
         }
+		System.out.println("Terima Kasih Telah Mencoba");
     }
  
 }
